bd341633257d843e3c861a2ba0f3fee586199fdf
Ramunas Zybartas <ramunzzz007@yahoo.es>
2022-01-26 20:26:57 +0200

    maked small changes

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -35,7 +35,7 @@ export const Home = () => {
     }
     return (
         <>
-        <PageTitle>Skills</PageTitle>
+        <PageTitle>Your Skills</PageTitle>
         <Wrapper>
             {
                 skills.map((skill) => (
